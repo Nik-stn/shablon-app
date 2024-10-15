7e6f871fd480be65d1e352020f9414ad60ceb1c6
KlinVid <stas.nik12@gmail.com>
2024-10-15 20:29:02 +0300

    Добавление навигации между страницами

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import CreateTemplate from './pages/CreateTemplate';
 import TemplatesList from './pages/TemplatesList';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,6 +9,10 @@ function App() {
     <Router>
       <div className="container mt-3">
         <h1>Форма Шаблонов</h1>
+        <nav>
+          <Link to="/" className="btn btn-primary mr-2">Список шаблонов</Link>
+          <Link to="/create" className="btn btn-secondary">Создать шаблон</Link>
+        </nav>
         <Routes>
           <Route path="/" element={<TemplatesList />} />
           <Route path="/create" element={<CreateTemplate />} />
