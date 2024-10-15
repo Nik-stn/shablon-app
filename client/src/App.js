ca31103c67f226a494104ce19cfa1dee4718f17d
KlinVid <stas.nik12@gmail.com>
2024-10-15 19:28:56 +0300

    Настройка маршрутизации и базовой структуры приложения

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import CreateTemplate from './pages/CreateTemplate';
+import TemplatesList from './pages/TemplatesList';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-3">
+        <h1>Форма Шаблонов</h1>
+        <Routes>
+          <Route path="/" element={<TemplatesList />} />
+          <Route path="/create" element={<CreateTemplate />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
