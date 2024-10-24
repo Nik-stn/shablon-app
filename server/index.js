833f048964a4289d0943292e863a66b1d3881b56
KlinVid <stas.nik12@gmail.com>
2024-10-24 16:50:37 +0300

    Добавил vercel.json

--- server/index.js
+++ server/index.js
@@ -1,30 +1,27 @@
-// server/index.js
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectDB = require('./config/db'); // Импортируем функцию подключения к базе данных
-const Template = require('./models/Template'); // Импортируем модель шаблона
+const connectDB = require('./config/db'); 
+const Template = require('./models/Template'); 
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Подключение к MongoDB
 connectDB();
 
 app.post('/api/templates', async (req, res) => {
   const { title, description } = req.body;
-  const newTemplate = new Template({ title, description }); // Используем модель Template
-  await newTemplate.save(); // Сохраняем новый шаблон в базе данных
+  const newTemplate = new Template({ title, description });
+  await newTemplate.save();
   res.status(201).json(newTemplate);
 });
 
 app.get('/api/templates', async (req, res) => {
-  const templates = await Template.find(); // Получаем все шаблоны из базы данных
+  const templates = await Template.find();
   res.json(templates);
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-});
+module.exports = (req, res) => {
+  app(req, res);
+};
