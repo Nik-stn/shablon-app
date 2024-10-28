72301c5c875e5807afcdf803e4837fa5094f5db8
KlinVid <stas.nik12@gmail.com>
2024-10-28 16:22:31 +0300

    Revert "Изменил url"
    This reverts commit 95cc7e26726766a4ba861b95f1ff0551fb214b71.

--- client/src/pages/TemplatesList.js
+++ client/src/pages/TemplatesList.js
@@ -9,7 +9,7 @@ const TemplatesList = () => {
   useEffect(() => {
     const fetchTemplates = async () => {
       try {
-        const res = await axios.get('https://vercel.com/stns-projects/shablon-app');
+        const res = await axios.get('http://localhost:5000/api/templates');
         setTemplates(res.data);
         setLoading(false);
       } catch (err) {
